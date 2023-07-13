3b33834297288dac0609d27a408393e3bbd69c64
Kevin Kim <kebonkim@gmail.com>
2023-07-12 23:28:51 -0700

    submitting functionality working

--- src/components/chat.jsx
+++ src/components/chat.jsx
@@ -1,11 +1,37 @@
-
+import {  useState } from 'react'
 
 
 import  './chat.css'
 
 function Chat() {
+    const [message, setMessage] = useState('')
+    const [submitted, setSubmitted] = useState(false)
+    
+    // const list = []
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        setSubmitted(true)
+
+        setTimeout(() => {
+            setSubmitted(false)
+        }, 5000)
+    }
+
+    // useEffect(() => {
+    //     list.push(message)
+    // }, [message, list])
+
+
     return (
-        <div id="chat_container">Chat Here</div>
+        <>
+        <form onSubmit={onSubmit} id="chat_container">
+            <div>Questions??</div>
+            <input type='text' placeholder='Type your question...' value={message} onChange={(event) => setMessage(event.target.value)}></input>
+            <button type='submit'>Send</button>
+        </form>
+        {submitted === true ? <div>{message}</div> : <div></div>}
+        </>
     )
 }
 
