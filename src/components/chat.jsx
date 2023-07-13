dc3f69b40b041c5ee3fef58711e79dcf50b39465
Kevin Kim <kebonkim@gmail.com>
2023-07-13 16:57:18 -0700

    fetch working

--- src/components/chat.jsx
+++ src/components/chat.jsx
@@ -1,21 +1,35 @@
 import {  useState } from 'react'
-
+import { Configuration, OpenAIApi } from 'openai'
 
 import  './chat.css'
 
 function Chat() {
     const [message, setMessage] = useState('')
+    const [result, setResult] = useState()
     const [submitted, setSubmitted] = useState(false)
     
     // const list = []
 
+    const apiKey = import.meta.env.VITE_API_KEY
+
+    
     const onSubmit = (event) => {
         event.preventDefault();
         setSubmitted(true)
-
-        setTimeout(() => {
-            setSubmitted(false)
-        }, 5000)
+        const chatgpt = new OpenAIApi(new Configuration({
+            apiKey: apiKey
+        }))
+    
+        chatgpt.createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            messages: [{role: 'user', content: message}]
+        }).then((res) => {
+        setResult(res.data.choices[0].message.content)
+        })
+        
+        // setTimeout(() => {
+        //     setSubmitted(false)
+        // }, 5000)
     }
 
     // useEffect(() => {
@@ -30,7 +44,7 @@ function Chat() {
             <input type='text' placeholder='Type your question...' value={message} onChange={(event) => setMessage(event.target.value)}></input>
             <button type='submit'>Send</button>
         </form>
-        {submitted === true ? <div>{message}</div> : <div></div>}
+        {submitted === true ? <div>{result}</div> : <div></div>}
         </>
     )
 }
